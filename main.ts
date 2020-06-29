20da3e98586bc5689eb13c8570ccc13c0e08fa9f
carlovanbaelen-wico <67262093+carlovanbaelen-wico@users.noreply.github.com>
2020-06-29 16:27:53 +0200

    Variabele Couter toevoegen

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
-basic.forever(function () {
-	
+let Counter = 0
+input.onButtonPressed(Button.A, function () {
+    Counter = 0
+})
+input.onButtonPressed(Button.B, function () {
+    Counter += 1
 })
