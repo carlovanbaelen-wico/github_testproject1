7c6bffb587ce7e5a9e6b939d28e1feacf0fb1234
carlovanbaelen-wico <67262093+carlovanbaelen-wico@users.noreply.github.com>
2020-06-29 16:34:07 +0200

    Verwijder knop B

--- main.ts
+++ main.ts
@@ -1,7 +1,6 @@
-let Counter = 0
 input.onButtonPressed(Button.A, function () {
-    Counter = 0
-})
-input.onButtonPressed(Button.B, function () {
     Counter += 1
+    basic.showString("" + (Counter))
 })
+let Counter = 0
+Counter = 0
